7a7ac0116ec3f442f94282189ee249331efa623f
Mohamed ShamsEldeen <74777795+30omda@users.noreply.github.com>
2025-10-27 21:03:36 +0300

    feat: enhance CompanionCard component and update main page with detailed companion entries

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,33 @@ const Page = () => {
     <main>
       <h1>Popular Companions</h1>
       <section className='home-section'>
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard 
+        id='123'
+        name='Countsy the Number Wizard'
+        topic='Derivatives & Integrals'
+        subject='science'
+        duration={30}
+        color='#e5d0ff'
+/>
+        <CompanionCard 
+        
+        id='456'
+        name='Verba the Vocabulary Builder'
+        topic='Language Learning'
+        subject='English Literature'
+        duration={30}
+        color='#BDE7FF'
+/>
+        <CompanionCard 
+        
+        id='789'
+        name='Codey the Logic Hacker'
+        topic='Intro to If-Else Statements'
+        subject='Coding'
+        duration={45}
+        color='#ffda6e'
+        
+/>
       </section>
       <section className='home-section'>
         <CompanionsList />
--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+interface CompanionCardProps {
+    id: string;
+    name: string;
+    topic: string;
+    subject: string;
+    duration: number;
+    color: string;
+}
 
-const CompanionCard = () => {
+const CompanionCard = ({ id, name, topic, subject, duration, color }: CompanionCardProps) => {
     return (
-        <div>
-            CompanionCard
-        </div>
+        <article className='companion-card' style={{backgroundColor: color}}></article>
     )
 }
 
-export default CompanionCard
+export default CompanionCard;
